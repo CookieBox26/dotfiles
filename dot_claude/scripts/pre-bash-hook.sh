f11ad24edd11a665e52a4d2efb55b6c682d718be
CookieBox26 <cookie-box@cookie-box.info>
2026-02-23 14:19:08 +0900

    Clarify permissions due to Claude behavior change

--- dot_claude/scripts/pre-bash-hook.sh
+++ dot_claude/scripts/pre-bash-hook.sh
@@ -6,6 +6,9 @@ command=$(printf '%s' "$json" | jq -r '.tool_input.command // ""')
 if echo "$command" | grep -qP '^pytest$'; then
   exit 0
 fi
+if echo "$command" | grep -qP '^bash -c \'source ~/.claude/scripts/ask.sh\''; then
+  exit 0
+fi
 
 script="$(pwd)/pre-bash-hook.sh"
 if [ -f "$script" ]; then
