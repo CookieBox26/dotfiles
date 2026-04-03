244d628b9b3c3e2cbb6bd90aa91dc8e390fa95d8
CookieBox26 <yurii_wednesday@yahoo.co.jp>
2026-04-04 00:15:08 +0900

    Fix ~/.claude/scripts/pre-bash-hook.sh

--- dot_claude/scripts/pre-bash-hook.sh
+++ dot_claude/scripts/pre-bash-hook.sh
@@ -3,22 +3,25 @@ set -euo pipefail
 json=$(cat)
 command=$(printf '%s' "$json" | jq -r '.tool_input.command // ""')
 
-if echo "$command" | grep -qP '^pytest$'; then
-  exit 0
-elif [[ "$command" =~ ^bash\ -c\ \'source\ ~/.claude/scripts/ask\.sh\' ]]; then
-  exit 0
-elif [[ "$command" =~ ^bash\ -c\ \'source\ ~/.claude/scripts/post-proc\.sh\'$ ]]; then
-  exit 0
-fi
+pattern="^(uv run )?pytest$"
+[[ "$command" =~ $pattern ]] && exit 0
+
+pattern="^bash -c 'source ~/\.claude/scripts/ask\.sh'"
+[[ "$command" =~ $pattern ]] && exit 0
+
+pattern="^bash -c 'source ~/\.claude/scripts/post-proc\.sh'$"
+[[ "$command" =~ $pattern ]] && exit 0
 
 script="$(pwd)/pre-bash-hook.sh"
 if [ -f "$script" ]; then
   bash "$script" "$command"
   exit $?
 fi
+
 script="$(pwd)/.claude/pre-bash-hook.sh"
 if [ -f "$script" ]; then
   bash "$script" "$command"
   exit $?
 fi
+
 exit 2
